33a4952ca29db3ce6f15e911c1e35f739b6ec403
Nick M <NMarine@StepwiseTesting.com>
2019-08-15 11:15:57 -0500

    Test modules for selecting vehicles and vehicle information.
    - added implemented implicit and explicit wait functionality
          - many driver.find_element functions have been replaced
            with self.wait.until functions
          - implicit wait is for 5 seconds
          - time.sleep has mostly been removed but is still used
            for certain tasks.

--- VehicleTestModules.py
+++ VehicleTestModules.py
@@ -3,26 +3,32 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 class VehicleTestModules:
 
     driver = webdriver
     CurrentModule = ""
     ErrorCount = 0
+    wait = WebDriverWait
 
     def __init__(self):
         self.driver = webdriver.Chrome(r"C:\drivers\chromedriver.exe")
         self.CurrentModule = "Initialization"
         self.ErrorCount = 0
+        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.implicitly_wait(5)
 
     def enter_name(self):
         try:
             self.CurrentModule = "enter_name"
             # enter first and last name
-            time.sleep(2)
 
-            FirstNameInput0 = self.driver.find_element_by_xpath("//input[@id='firstName']")
+            FirstNameInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='firstName']")))
             ActionChains(self.driver).move_to_element(FirstNameInput0).click().send_keys("Hello").send_keys(Keys.TAB).send_keys(
                 "World").send_keys(Keys.TAB).send_keys(Keys.ENTER).perform()
         except Exception as err:
@@ -32,18 +38,15 @@ class VehicleTestModules:
         try:
             self.CurrentModule = "skip_help_page"
             # skip the initial "how can we help" page
-            time.sleep(2)
 
-            ZipInput0 = self.driver.find_element_by_xpath("//input[@id='zip']")
+            ZipInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='zip']")))
             ActionChains(self.driver).move_to_element(ZipInput0).click().send_keys("60103").send_keys(
                 Keys.TAB).send_keys(
                 Keys.ENTER).perform()
 
             time.sleep(2)
             butbar = self.driver.find_element_by_class_name("button-bar")
-
             SkipA0 = butbar.find_element_by_class_name("skip-collect-intent.link--primary")
-
             ActionChains(self.driver).move_to_element(SkipA0).click().perform()
 
             self.go_next()
@@ -54,15 +57,14 @@ class VehicleTestModules:
         try:
             self.CurrentModule = "enter_DOB"
             # enter DOB
-            time.sleep(2)
 
-            MonthDOB0 = self.driver.find_element_by_xpath("//input[@id='date-monthdob']")
+            MonthDOB0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='date-monthdob']")))
             ActionChains(self.driver).move_to_element(MonthDOB0).click().send_keys("06").perform()
 
-            DayDOB0 = self.driver.find_element_by_xpath("//input[@id='date-daydob']")
+            DayDOB0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='date-daydob']")))
             ActionChains(self.driver).move_to_element(DayDOB0).click().send_keys("06").perform()
 
-            YearDOB0 = self.driver.find_element_by_xpath("//input[@id='date-yeardob']")
+            YearDOB0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='date-yeardob']")))
             ActionChains(self.driver).move_to_element(YearDOB0).click().send_keys("1996").send_keys(Keys.TAB).send_keys(
                 Keys.ENTER).perform()
         except Exception as err:
@@ -72,15 +74,14 @@ class VehicleTestModules:
         try:
             self.CurrentModule = "enter_address"
             # enter street address info
-            time.sleep(2)
 
-            StreetInput0 = self.driver.find_element_by_xpath("//input[@id='street']")
+            StreetInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='street']")))
             ActionChains(self.driver).move_to_element(StreetInput0).click().send_keys("123 Whatever St.").perform()
 
-            AptInput0 = self.driver.find_element_by_xpath("//input[@id='apt']")
+            AptInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='apt']")))
             ActionChains(self.driver).move_to_element(AptInput0).click().send_keys("312").perform()
 
-            ZipInput1 = self.driver.find_element_by_xpath("//input[@id='zip']")
+            ZipInput1 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='zip']")))
             ActionChains(self.driver).move_to_element(ZipInput1).click().send_keys("60103").send_keys(Keys.TAB).send_keys(
                 Keys.ENTER).perform()
 
@@ -91,9 +92,8 @@ class VehicleTestModules:
         try:
             self.CurrentModule = "verify_address"
             # verify address
-            time.sleep(7)
-
-            OriginalAddressLabel0 = self.driver.find_element_by_xpath("//label[@for='originalAddress']")
+            time.sleep(3)
+            OriginalAddressLabel0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='originalAddress']")))
             ActionChains(self.driver).move_to_element(OriginalAddressLabel0).click().send_keys(Keys.TAB).send_keys(
                 Keys.ENTER).perform()
         except Exception as err:
@@ -103,8 +103,8 @@ class VehicleTestModules:
         # have you moved in the last 2 months (put no)
         try:
             self.CurrentModule = "have_you_moved"
-            time.sleep(6)
-            HasMovedInLast2MonthsLabel1 = self.driver.find_element_by_xpath("//label[@for='hasMovedInLast2Months1']")
+            time.sleep(3)
+            HasMovedInLast2MonthsLabel1 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='hasMovedInLast2Months1']")))
             ActionChains(self.driver).move_to_element(HasMovedInLast2MonthsLabel1).click().send_keys(Keys.TAB).send_keys(
                 Keys.ENTER).perform()
         except Exception as err:
@@ -125,14 +125,11 @@ class VehicleTestModules:
     def select_specific_vehicle(self, year_index, make_index, model_index):
         try:
             self.CurrentModule = "select_specific_vehicle"
-            time.sleep(4)
-            VehicleYearSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleYear']"))
+            VehicleYearSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleYear']"))))
             VehicleYearSelect0.select_by_index(year_index)
-            time.sleep(1)
-            VehicleMakeSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleMake']"))
+            VehicleMakeSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleMake']"))))
             VehicleMakeSelect0.select_by_index(make_index)
-            time.sleep(1)
-            VehicleModelSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleModel']"))
+            VehicleModelSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleModel']"))))
             VehicleModelSelect0.select_by_index(model_index)
 
             self.go_next()
@@ -145,9 +142,8 @@ class VehicleTestModules:
         self.CurrentModule = "select_vehicles"
 
         ### NOW WE CAN TEST FOR THE ADDING OF VEHCILES ###
-        time.sleep(4)
 
-        VehicleYearSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleYear']"))
+        VehicleYearSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleYear']"))))
 
         YearOption = 0
 
@@ -155,29 +151,27 @@ class VehicleTestModules:
             try:
                 VehicleYearSelect0.select_by_index(YearOption)
             except:
-                VehicleYearSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleYear']"))
+                VehicleYearSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleYear']"))))
                 VehicleYearSelect0.select_by_index(YearOption)
 
-            time.sleep(1)
-            VehicleMakeSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleMake']"))
+            VehicleMakeSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleMake']"))))
 
             MakeOption = 1
             while MakeOption < len(VehicleMakeSelect0.options):
                 try:
                     VehicleMakeSelect0.select_by_index(MakeOption)
                 except:
-                    VehicleMakeSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleMake']"))
+                    VehicleMakeSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleMake']"))))
                     VehicleMakeSelect0.select_by_index(MakeOption)
 
-                time.sleep(1)
-                VehicleModelSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleModel']"))
+                VehicleModelSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleModel']"))))
 
                 ModelOption = 1
                 while ModelOption < len(VehicleModelSelect0.options):  # it really starts to slow down over here
                     try:
                         VehicleModelSelect0.select_by_index(ModelOption)
                     except:
-                        VehicleModelSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleModel']"))
+                        VehicleModelSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleModel']"))))
                         VehicleModelSelect0.select_by_index(ModelOption)
 
                     # time.sleep(1)
@@ -189,16 +183,16 @@ class VehicleTestModules:
         try:
             self.CurrentModule = "add_vehicle_pre1981"
 
-            VehicleYearSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='vehicleYear']"))
+            VehicleYearSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='vehicleYear']"))))
             VehicleYearSelect0.select_by_index(len(VehicleYearSelect0.options) - 1)
 
-            ActualVehicleYearInput0 = self.driver.find_element_by_xpath("//input[@id='actualVehicleYear']")
+            ActualVehicleYearInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='actualVehicleYear']")))
             ActionChains(self.driver).move_to_element(ActualVehicleYearInput0).click().send_keys(str(year)).perform()
 
-            OtherMakeInput0 = self.driver.find_element_by_xpath("//input[@id='otherMake']")
+            OtherMakeInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='otherMake']")))
             ActionChains(self.driver).move_to_element(OtherMakeInput0).click().send_keys(str(make)).perform()
 
-            OtherModelInput0 = self.driver.find_element_by_xpath("//input[@id='otherModel']")
+            OtherModelInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='otherModel']")))
             ActionChains(self.driver).move_to_element(OtherModelInput0).click().send_keys(str(model)).perform()
 
             self.go_next()
@@ -209,8 +203,7 @@ class VehicleTestModules:
         try:
             self.CurrentModule = "select_antitheft_devices"
 
-            time.sleep(3)
-            AntiTheftDeviceSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='antiTheftDevice']"))
+            AntiTheftDeviceSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='antiTheftDevice']"))))
 
             ATDOption = 0
 
@@ -225,8 +218,7 @@ class VehicleTestModules:
     def select_specific_antitheft_device(self, index):
         try:
             self.CurrentModule = "select_specific_antitheft_device"
-            time.sleep(3)
-            AntiTheftDeviceSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='antiTheftDevice']"))
+            AntiTheftDeviceSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='antiTheftDevice']"))))
             AntiTheftDeviceSelect0.select_by_index(index)
 
             self.go_next()
@@ -238,16 +230,15 @@ class VehicleTestModules:
     def select_ownership(self, index):
         try:
             self.CurrentModule = "select_ownership"
-            time.sleep(3)
 
             if index == 0:
-                VehicleOwned0 = self.driver.find_element_by_xpath("//label[@for='vehicleOwned0']")
+                VehicleOwned0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='vehicleOwned0']")))
                 ActionChains(self.driver).move_to_element(VehicleOwned0).click().perform()
             if index == 1:
-                VehicleOwned1 = self.driver.find_element_by_xpath("//label[@for='vehicleOwned1']")
+                VehicleOwned1 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='vehicleOwned1']")))
                 ActionChains(self.driver).move_to_element(VehicleOwned1).click().perform()
             if index == 2:
-                VehicleOwned2 = self.driver.find_element_by_xpath("//label[@for='vehicleOwned2']")
+                VehicleOwned2 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='vehicleOwned2']")))
                 ActionChains(self.driver).move_to_element(VehicleOwned2).click().perform()
 
             self.go_next()
@@ -261,18 +252,17 @@ class VehicleTestModules:
         try:
 
             self.CurrentModule = "select_primary_use"
-            time.sleep(3)
 
             if index == 0:
-                PrimaryUse0 = self.driver.find_element_by_xpath("//label[@for='primaryUse0']")
+                PrimaryUse0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='primaryUse0']")))
                 ActionChains(self.driver).move_to_element(PrimaryUse0).click().perform()
                 self.primary_use_commute(1, 200)
             if index == 1:
-                PrimaryUse1 = self.driver.find_element_by_xpath("//label[@for='primaryUse1']")
+                PrimaryUse1 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='primaryUse1']")))
                 ActionChains(self.driver).move_to_element(PrimaryUse1).click().perform()
                 self.go_next()
             if index == 2:
-                PrimaryUse2 = self.driver.find_element_by_xpath("//label[@for='primaryUse2']")
+                PrimaryUse2 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='primaryUse2']")))
                 ActionChains(self.driver).move_to_element(PrimaryUse2).click().perform()
                 self.primary_use_business(1)
 
@@ -283,11 +273,11 @@ class VehicleTestModules:
     def primary_use_commute(self, index, miles):
         try:
             self.CurrentModule = "primary_use_commute"
-            time.sleep(1)
-            DaysDrivenSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='daysDriven']"))
+
+            DaysDrivenSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='daysDriven']"))))
             DaysDrivenSelect0.select_by_index(index)
 
-            MilesDrivenInput0 = self.driver.find_element_by_xpath("//input[@id='milesDriven']")
+            MilesDrivenInput0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@id='milesDriven']")))
             ActionChains(self.driver).move_to_element(MilesDrivenInput0).click().send_keys(str(miles)).perform()
 
             self.go_next()
@@ -299,8 +289,8 @@ class VehicleTestModules:
     def select_annual_mileage(self, index):
         try:
             self.CurrentModule = "select_annual_mileage"
-            time.sleep(3)
-            AnnualMileageSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='annualMileage']"))
+
+            AnnualMileageSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='annualMileage']"))))
             AnnualMileageSelect0.select_by_index(index)
 
             self.go_next()
@@ -311,8 +301,8 @@ class VehicleTestModules:
     def primary_use_business(self, index):
         try:
             self.CurrentModule = "primary_use_business"
-            time.sleep(1)
-            TypeOfBusinessUseSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='typeOfBusinessUse']"))
+
+            TypeOfBusinessUseSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='typeOfBusinessUse']"))))
             TypeOfBusinessUseSelect0.select_by_index(index)
 
             self.go_next()
@@ -322,9 +312,8 @@ class VehicleTestModules:
     def select_body_styles(self):
         try:
             self.CurrentModule = "select_body_styles"
-            time.sleep(1)
 
-            BodyStylesSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='bodyStyles']"))
+            BodyStylesSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='bodyStyles']"))))
 
             BSSOption = 0
             while BSSOption < len(BodyStylesSelect0.options):
@@ -337,9 +326,8 @@ class VehicleTestModules:
     def select_specific_body_style(self, index):
         try:
             self.CurrentModule = "select_specific_body_styles"
-            time.sleep(1)
 
-            BodyStylesSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='bodyStyles']"))
+            BodyStylesSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='bodyStyles']"))))
 
             BodyStylesSelect0.select_by_index(index)
 
@@ -351,9 +339,8 @@ class VehicleTestModules:
 
         try:
             self.CurrentModule = "select_new_costs"
-            time.sleep(1)
 
-            CostNewValueSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='costNewValue']"))
+            CostNewValueSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='costNewValue']"))))
 
             CNVSOption = 0
             while CNVSOption < len(CostNewValueSelect0.options):
@@ -367,9 +354,8 @@ class VehicleTestModules:
 
         try:
             self.CurrentModule = "select_specific_new_costs"
-            time.sleep(1)
 
-            CostNewValueSelect0 = Select(self.driver.find_element_by_xpath("//select[@id='costNewValue']"))
+            CostNewValueSelect0 = Select(self.wait.until(ec.element_to_be_clickable((By.XPATH, "//select[@id='costNewValue']"))))
             CostNewValueSelect0.select_by_index(index)
 
             self.go_next()
@@ -380,23 +366,22 @@ class VehicleTestModules:
         try:
 
             self.CurrentModule = "select_antilock_brakes"
-            time.sleep(3)
 
             if index == 0:
-                AntilockBrakesLabel0 = self.driver.find_element_by_xpath("//label[@for='hasAntilockBrakes0']")
+                AntilockBrakesLabel0 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='hasAntilockBrakes0']")))
                 ActionChains(self.driver).move_to_element(AntilockBrakesLabel0).click().perform()
                 self.go_next()
             if index == 1:
-                AntilockBrakesLabel1 = self.driver.find_element_by_xpath("//label[@for='hasAntilockBrakes1']")
+                AntilockBrakesLabel1 = self.wait.until(ec.element_to_be_clickable((By.XPATH, "//label[@for='hasAntilockBrakes1']")))
                 ActionChains(self.driver).move_to_element(AntilockBrakesLabel1).click().perform()
                 self.go_next()
 
         except Exception as err:
             self.error_message(err)
 
+    # these functions get you to the page to add vehicles
     def get_to_vehicle_page(self):
         self.driver.get("https://www.geico.com/")
-        time.sleep(2)
         self.skip_help_page()
         self.enter_name()
         self.enter_DOB()
