d2fb3c2ce13bc97efadad14e9340fd9fafae4ff1
garytsokatos <54143076+garytsokatos@users.noreply.github.com>
2019-08-21 11:31:09 -0500

    wrote scripts up to test case E05.11

--- GeicoTestScripts.py
+++ GeicoTestScripts.py
@@ -74,7 +74,6 @@ try:
     Test_A01.next_button_0()
     print("test case A01 ran successfully")
     Test_A01.close_browser()
-
 except Exception as err:
     print('Test A01' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
     print(err.args)
@@ -222,227 +221,6 @@ except Exception as err:
     print(err.__module__)
 
 
-
-#TC_G01 - ADDING NEW VEHICLE INFORMATION/YEAR, MAKE, MODEL (1981 - 2020)
-try:
-    Test_G01 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_G01.select_specific_vehicle(1, 2, 3)
-    print("test G01 ran successfully")
-    CloseBrowser.close_browser()
-except Exception as err:
-    print('Test G01' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test G01:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_G02 - ADDING NEW VEHICLE INFORMATION/YEAR, MAKE, MODEL (PRE 1981)
-try:
-    Test_G02 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_G02.select_specific_vehicle(1, 2, 3)
-    print("test G02 ran successfully")
-    CloseBrowser.close_browser()
-except Exception as err:
-    print('Test G02' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test G02:")
-    print(err.__module__)
-    print(str(err))
-
-# TC_G03 - ADDING NEW VEHICLE INFORMATION/YEAR, MAKE, MODEL (PRE 1981)
-try:
-    Test_G03 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_G03.add_vehicle_pre1981(1, 2, 3)
-    print("test G03 ran successfully")
-    CloseBrowser.close_browser()
-except Exception as err:
-    print('Test G03' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test G03:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_H01 - SPECIAL FEATURES: BODY STYLE
-try:
-    Test_H01 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_H01.select_body_styles()
-    print("test H01 ran successfully")
-    CloseBrowser.close_browser()
-except Exception as err:
-    print('Test H01' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test H01:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_H02 - SPECIAL FEATURES: ANTI-THEFT DEVICE
-try:
-    Test_H02 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_H02.select_antitheft_devices()
-    print("test H02 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test H02' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test H02:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_H03 - SPECIAL FEATURES: ANTI-LOCK BRAKES
-try:
-    Test_H03 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_H03.select_antilock_brakes()
-    print("test H03 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test H03' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test H03:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_H04 - SPECIAL FEATURES: NEW COST
-try:
-    Test_H04 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_H04.select_new_costs()
-    print("test H01 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test H04' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test H04:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_I01 - VEHICLE OWNERSHIP - OWNED
-try:
-    Test_I01 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_I01.select_ownership(0, 1, 2)
-    print("test I01 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test I01' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test I01:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_I02 - VEHICLE OWNERSHIP - FINANCED
-try:
-    Test_I02 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_I02.select_ownership(0, 1, 2)
-    print("test I02 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test I02' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test I02:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_I03 - VEHICLE OWNERSHIP - LEASED
-try:
-    Test_I03 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_I03.select_ownership(0, 1, 2)
-    print("test I03 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test I03' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test I03:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_J01 - ADDING PRIMARY USE - COMMUTE
-try:
-    Test_J01 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_J01.select_ownership(0, 1, 2)
-    print("test J01 ran successfully")
-    Test_J01.close_browser()
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test J01' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test J01:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_J02 - ADDING PRIMARY USE - PLEASURE
-try:
-    Test_J02 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_J02.select_ownership(0, 1, 2)
-    print("test J02 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test J02' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test J02:")
-    print(err.__module__)
-    print(str(err))
-
-#TC_J03 - ADDING PRIMARY USE - BUSINESS
-try:
-    Test_J03 = VehicleTestModules
-    CloseBrowser = Auto_Geico_Test()
-    # preconditionD(Test_E0)
-
-    Test_J03.select_ownership(0, 1, 2)
-    print("test J03 ran successfully")
-    CloseBrowser.close_browser()
-
-except Exception as err:
-    print('Test J03' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
-    print(err.args)
-    print("An error occured while running automation test for Geico Test J03:")
-    print(err.__module__)
-    print(str(err))
-
-
-
 #TC_E01 - DRIVER INFORMATION - SELECT GENDER
 try:
     Test_E01 = Auto_Geico_Test()
@@ -466,11 +244,11 @@ try:
 
     Test_E02.marital_status_0()
     Test_E02.social_security_number_0()
-    Test_E02.next_button_5()
+    Test_E02.next_button_x()
     Test_E02.social_security_number_2()
-    Test_E02.next_button_5()
+    Test_E02.next_button_x()
     Test_E02.social_security_number_1()
-    Test_E02.next_button_5()
+    Test_E02.next_button_x()
     print("test E02 ran successfully")
     Test_E02.close_browser()
 
@@ -487,7 +265,7 @@ try:
     # preconditionX(Test_E03)
 
     Test_E03.home_ownership_0()
-    Test_E03.next_button_6()
+    Test_E03.next_button_x()
     print("test E03 ran successfully")
     Test_E03.close_browser()
 
@@ -504,10 +282,13 @@ try:
     # preconditionX(Test_E04)
 
     Test_E04o1.current_insured_status_1()
-    Test_E04o1.next_button_6()
-    Test_E04o1.current_insurance_disclosure_0
-    Test_E04o2.previous_insurance_disclosure_3()
-    Test_E04o1.next_button_6()
+    Test_E04o1.next_button_x()
+    Test_E04o1.current_insurance_disclosure_0()
+    Test_E04o1.current_insurance_disclosure_1()
+    Test_E04o1.previous_insurance_disclosure_3()
+    Test_E04o1.next_button_x()
+    Test_E04o1.driving_history_1()
+    Test_E04o1.next_button_x()
     print("test E04o1 ran successfully")
     Test_E04o1.close_browser()
 
@@ -524,7 +305,9 @@ try:
     # preconditionX(Test_E04)
 
     Test_E04o2.current_insured_status_2()
-    Test_E04o2.next_button_6()
+    Test_E04o2.next_button_x()
+    Test_E04o2.driving_history_1()
+    Test_E04o2.next_button_x()
     print("test E04o2 ran successfully")
     Test_E04o2.close_browser()
 
@@ -541,11 +324,13 @@ try:
     # preconditionX(Test_E04)
 
     Test_E04o3.current_insured_status_3()
-    Test_E04o3.next_button_6()
+    Test_E04o3.next_button_x()
     Test_E04o3.previous_insurance_disclosure_0()
     Test_E04o3.previous_insurance_disclosure_1()
     Test_E04o3.previous_insurance_disclosure_3()
-    Test_E04o3.next_button_6()
+    Test_E04o3.next_button_x()
+    Test_E04o3.driving_history_1()
+    Test_E04o3.next_button_x()
     print("test E04o3 ran successfully")
     Test_E04o3.close_browser()
 
@@ -562,7 +347,7 @@ try:
     # preconditionX(Test_E04)
 
     Test_E04o4.current_insured_status_4()
-    Test_E04o4.next_button_6()
+    Test_E04o4.next_button_x()
     print("test E04o4 ran successfully")
     Test_E04o4.close_browser()
 
@@ -573,18 +358,211 @@ except Exception as err:
     print(err.__module__)
     print(str(err))
 
+#TC_XX01 - DRIVER INFORMATION - EDUCATION
+try:
+    Test_XX01 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
 
+    Test_XX01.education_level_0()
+    Test_XX01.next_button_x()
+    print("test E04o4 ran successfully")
 
+except Exception as err:
+    print('Test XX01' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test XX01:")
+    print(err.__module__)
+    print(str(err))
 
+#TC_E05o1 - DRIVER INFORMATION - EMPLOYMENT STATUS - "A PRIVATE COMPANY/ORGANIZATION OR SELF EMPLOYED"
+try:
+    Test_E05o1 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
 
+    Test_E05o1.employment_status_0()
+    Test_E05o1.next_button_x()
+    Test_E05o1.current_occupation_0()
+    Test_E05o1.current_occupation_1()
+    Test_E05o1.next_button_x()
+    print("test E05o1 ran successfully")
 
+except Exception as err:
+    print('Test E05o1' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o1:")
+    print(err.__module__)
+    print(str(err))
 
+#TC_E05o2 - DRIVER INFORMATION - EMPLOYMENT STATUS - "ACTIVE MILITARY DUTY"
+try:
+    Test_E05o2 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
 
+    Test_E05o2.employment_status_0()
+    Test_E05o2.employment_status_1()
+    Test_E05o2.employment_status_2()
+    Test_E05o2.next_button_x()
+    print("test E05o2 ran successfully")
 
+except Exception as err:
+    print('Test E05o2' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o2:")
+    print(err.__module__)
+    print(str(err))
 
+#TC_E05o3 - DRIVER INFORMATION - EMPLOYMENT STATUS - "FEDERAL GOVERNMENT AND POSTAL SERVICE"
+try:
+    Test_E05o3 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
 
+    Test_E05o3.employment_status_0()
+    Test_E05o3.employment_status_3()
+    Test_E05o3.next_button_x()
+    print("test E05o3 ran successfully")
 
+except Exception as err:
+    print('Test E05o3' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o3:")
+    print(err.__module__)
+    print(str(err))
 
+#TC_E05o4 - DRIVER INFORMATION - EMPLOYMENT STATUS - "A STAE/LOCAL/MUNICIPAL GOVERNMENT"
+try:
+    Test_E05o4 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o4.employment_status_0()
+    Test_E05o4.employment_status_3()
+    Test_E05o4.next_button_x()
+    print("test E05o4 ran successfully")
+
+except Exception as err:
+    print('Test E05o4' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o4:")
+    print(err.__module__)
+    print(str(err))
+
+#TC_E05o5 - DRIVER INFORMATION - EMPLOYMENT STATUS - "I AM A FULL TIME STUDENT"
+try:
+    Test_E05o5 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o5.employment_status_0()
+    Test_E05o5.next_button_x()
+    print("test E05o4 ran successfully")
+
+except Exception as err:
+    print('Test E05o5' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o5:")
+    print(err.__module__)
+    print(str(err))
+
+#TC_E05o6 - DRIVER INFORMATION - EMPLOYMENT STATUS - "I AM CURRENTLY A HOMEMAKER"
+try:
+    Test_E05o6 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o6.employment_status_0()
+    Test_E05o6.next_button_x()
+    print("test E05o6 ran successfully")
+
+except Exception as err:
+    print('Test E05o6' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o6:")
+    print(err.__module__)
+    print(str(err))
+
+#TC_E05o7 - DRIVER INFORMATION - EMPLOYMENT STATUS - "I AM NOT CURRENTLY EMPLOYED"
+try:
+    Test_E05o7 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o7.employment_status_0()
+    Test_E05o7.next_button_x()
+    print("test E05o7 ran successfully")
+
+except Exception as err:
+    print('Test E05o7' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05O7:")
+    print(err.__module__)
+    print(str(err))
+
+#TC_E05o8 - DRIVER INFORMATION - EMPLOYMENT STATUS - "RETIRED PRIVATE COMPANY/ORGANIZATION OR SELF EMPLOYED"
+try:
+    Test_E05o7 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o7.employment_status_0()
+    Test_E05o7.next_button_x()
+    Test_E05o7.current_occupation_0()
+    Test_E05o7.current_occupation_1()
+    Test_E05o7.next_button_x()
+    print("test E05o7 ran successfully")
+
+except Exception as err:
+    print('Test E05o7' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05O7:")
+    print(err.__module__)
+    print(str(err))
+
+#TC_E05o9 - DRIVER INFORMATION - EMPLOYMENT STATUS - "RETIRED MILITARY"
+try:
+    Test_E05o9 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o9.employment_status_0()
+    Test_E05o9.employment_status_1()
+    Test_E05o9.employment_status_2()
+    Test_E05o9.next_button_x()
+    print("test E05o9 ran successfully")
+
+except Exception as err:
+    print('Test E05o9' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o9:")
+    print(err.__module__)
+    print(str(err))
+
+#TC_E05o10 - DRIVER INFORMATION - EMPLOYMENT STATUS - "RETIRED FEDERAL GOVERNMENT AND POSTAL SERVICE"
+try:
+    Test_E05o10 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o10.employment_status_0()
+    Test_E05o10.employment_status_3()
+    Test_E05o10.next_button_x()
+    print("test E05o10 ran successfully")
+
+except Exception as err:
+    print('Test E05o10' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o10:")
+    print(err.__module__)
+    print(str(err))
+
+#TC_E05o11 - DRIVER INFORMATION - EMPLOYMENT STATUS - "RETIRED STAE/LOCAL/MUNICIPAL GOVERNMENT"
+try:
+    Test_E05o11 = Auto_Geico_Test()
+    # preconditionX(Test_E04)
+
+    Test_E05o11.employment_status_0()
+    Test_E05o11.employment_status_3()
+    Test_E05o11.next_button_x()
+    print("test E05o11 ran successfully")
+
+except Exception as err:
+    print('Test E05o11' + ' test automation observed an error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+    print(err.args)
+    print("An error occured while running automation test for Geico Test E05o10:")
+    print(err.__module__)
+    print(str(err))
 
 
 
